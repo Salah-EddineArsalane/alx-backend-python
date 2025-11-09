fc65264acaeebd0b5b7ad656cf1b778701557e7a
Salah-EddineArsalane <arsalane.salaheddine@gmail.com>
2025-11-09 23:22:21 +0100

    Create 0-databaseconnection.py

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+class DatabaseConnection:
+    """Custom context manager for handling DB connections"""
+    def __enter__(self):
+        self.conn = sqlite3.connect('users.db')
+        self.cursor = self.conn.cursor()
+        print("[INFO] Database connection opened.")
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+            print("[ERROR] Exception occurred:", exc_val)
+        self.conn.close()
+        print("[INFO] Database connection closed.")
+
+# Using the custom context manager
+with DatabaseConnection() as cursor:
+    cursor.execute("SELECT * FROM users")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
