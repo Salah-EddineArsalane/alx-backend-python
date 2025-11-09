325420e66826e0c603175bf1d3d6fae743a91ad5
Salah-EddineArsalane <arsalane.salaheddine@gmail.com>
2025-11-09 23:03:14 +0100

    Create 0-stream_users.py

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,9 @@
+def stream_users():
+    import seed
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+    for row in cursor:
+        yield row
+    cursor.close()
+    connection.close()
